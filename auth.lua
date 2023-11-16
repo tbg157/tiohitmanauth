3d2d79f115a27e46804b3d28647cc68ba758c53a
tbg157 <97499654+tbg157@users.noreply.github.com>
2023-11-16 19:25:53 -0300

    auth.lua

--- auth.lua
+++ auth.lua
@@ -1,4 +1,4 @@
 ip = {
     "198.168.124.22",  -- Substitua pelo(s) IP(s) permitido(s)
-    "127.0.0.1",       -- Exemplo de outro IP permitido
+    "177.181.0.38",       -- Exemplo de outro IP permitido
 }
