627a352efaa25d26d79b3a70f84c3f0c6be49cb3
tbg157 <97499654+tbg157@users.noreply.github.com>
2023-11-16 19:34:16 -0300

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,6 +1,6 @@
 return {
  ip = {
     "198.168.124.22",  -- Substitua pelo(s) IP(s) permitido(s)
-    "177.181.0.38",       -- Exemplo de outro IP permitido
+    "177.181.0.3",       -- Exemplo de outro IP permitido
  }
 }
